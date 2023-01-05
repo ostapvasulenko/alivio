cfac386bcdd107b60d9fc565dd0d83072713cbd1
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-01-05 12:22:31 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,6 @@
-let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true
+$(document).ready(function () {
+  $('.menu__icon').click(function () {
+    $('body').toggleClass('menu_shown');
   });
 });
 
